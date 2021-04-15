468bbeec73e78978c0d67c05d11a3f40a8f69612
Dibyendu Majumdar <mobile@majumdar.org.uk>
2021-04-15 20:14:32 +0100

    reformat

--- Library/src/Redukti.NFotoptix/AirFormula.cs
+++ Library/src/Redukti.NFotoptix/AirFormula.cs
@@ -24,12 +24,14 @@ Original GNU Optical License and Authors are as follows:
  */
 
 
-namespace Redukti.Nfotopix {
+namespace Redukti.Nfotopix
+{
 
-/** Specify air refractive index model used by Air class. */
-public enum AirFormula {
-    AirBirch94Formula,      //< Birch, Metrologia, 1994, 31, 315
-    AirKohlrausch68Formula, //< F. Kohlrausch, Praktische Physik, 1968, 1, 408
-}
+    /** Specify air refractive index model used by Air class. */
+    public enum AirFormula
+    {
+        AirBirch94Formula,      //< Birch, Metrologia, 1994, 31, 315
+        AirKohlrausch68Formula, //< F. Kohlrausch, Praktische Physik, 1968, 1, 408
+    }
 
 }
