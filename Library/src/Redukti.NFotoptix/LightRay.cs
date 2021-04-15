947abd0b5c547330d947aec8b517d590d1d9d4e4
Dibyendu Majumdar <mobile@majumdar.org.uk>
2021-04-15 22:14:52 +0100

    reformat

--- Library/src/Redukti.NFotoptix/LightRay.cs
+++ Library/src/Redukti.NFotoptix/LightRay.cs
@@ -23,65 +23,74 @@ Original GNU Optical License and Authors are as follows:
       Author: Alexandre Becoulet
  */
 
-namespace Redukti.Nfotopix {
+namespace Redukti.Nfotopix
+{
 
 
-/**
- * Describe a ray of light
- * <p>
- * This class is used to describe a ray of light. It contains
- * geometrical and physical information about a light ray.
- */
-public class LightRay {
-    protected Vector3Pair _ray;
-    protected double _wavelen;
-    protected double _intensity;
+    /**
+     * Describe a ray of light
+     * <p>
+     * This class is used to describe a ray of light. It contains
+     * geometrical and physical information about a light ray.
+     */
+    public class LightRay
+    {
+        protected Vector3Pair _ray;
+        protected double _wavelen;
+        protected double _intensity;
 
-    public LightRay(Vector3Pair ray, double intensity, double wavelen) {
-        this._ray = ray;
-        this._intensity = intensity;
-        this._wavelen = wavelen;
-    }
-
-    public LightRay(Vector3Pair ray): this(ray, 1.0, SpectralLine.d)
+        public LightRay(Vector3Pair ray, double intensity, double wavelen)
         {
-        
-    }
+            this._ray = ray;
+            this._intensity = intensity;
+            this._wavelen = wavelen;
+        }
 
-    public LightRay(LightRay ray): this(ray._ray, ray._intensity, ray._wavelen)
+        public LightRay(Vector3Pair ray) : this(ray, 1.0, SpectralLine.d)
         {
-        
-    }
 
-    public LightRay(): this(Vector3Pair.position_000_001)
+        }
+
+        public LightRay(LightRay ray) : this(ray._ray, ray._intensity, ray._wavelen)
         {
-        
-    }
 
-    public void set_intensity(double intensity) {
-        this._intensity = intensity;
-    }
+        }
 
-    public void set_wavelen(double wavelen) {
-        this._wavelen = wavelen;
-    }
+        public LightRay() : this(Vector3Pair.position_000_001)
+        {
 
-    public Vector3Pair get_ray() {
-        return _ray;
-    }
+        }
 
-    public double get_intensity() {
-        return _intensity;
-    }
+        public void set_intensity(double intensity)
+        {
+            this._intensity = intensity;
+        }
 
-    public double get_wavelen() {
-        return _wavelen;
-    }
+        public void set_wavelen(double wavelen)
+        {
+            this._wavelen = wavelen;
+        }
 
-    public override string ToString() {
-        return "LightRay{wavelen=" + _wavelen + ",origin=" + _ray.origin() + ",direction=" + _ray.direction() + '}';
-    }
+        public Vector3Pair get_ray()
+        {
+            return _ray;
+        }
 
-}
+        public double get_intensity()
+        {
+            return _intensity;
+        }
+
+        public double get_wavelen()
+        {
+            return _wavelen;
+        }
+
+        public override string ToString()
+        {
+            return "LightRay{wavelen=" + _wavelen + ",origin=" + _ray.origin() + ",direction=" + _ray.direction() + '}';
+        }
+
+    }
 
 }
--- Library/src/Redukti.NFotoptix/SpectralLine.cs
+++ Library/src/Redukti.NFotoptix/SpectralLine.cs
@@ -24,126 +24,130 @@ Original GNU Optical License and Authors are as follows:
  */
 
 
-namespace Redukti.Nfotopix {
+namespace Redukti.Nfotopix
+{
 
 
-/**
- Describe a spectral line
+    /**
+     Describe a spectral line
 
- This class can be used to describe a spectral line. It contains
- constants for wave length of standard rays
- */
-public class SpectralLine {
-    double _wavelen;
-    double _intensity;
-
-    /** red colored line at 645nm */
-    public static readonly double red = 645.0;
-    /** green colored line at 510nm */
-    public static readonly double green = 510.0;
-    /** blue colored line at 440nm */
-    public static readonly double blue = 440.0;
-
-    /** infrared mercury line at 1013.98nm */
-    public static readonly double t = 1013.98;
-    /** infrared cesium line at 852.11nm */
-    public static readonly double s = 852.11;
-    /** red helium line at 706.5188nm */
-    public static readonly double r = 706.5188;
-    /** red hydrogen line at 656.2725nm */
-    public static readonly double C = 656.2725;
-    /** red cadmium line at 643.8469nm */
-    public static readonly double C_ = 643.8469; // C'
-    /** yellow sodium line at 589.2938nm */
-    public static readonly double D = 589.2938;
-    /** yellow helium line at 587.5618nm */
-    public static readonly double d = 587.5618;
-    /** green mercury line at 546.074nm */
-    public static readonly double e = 546.074;
-    /** blue hydrogen line at 486.1327nm */
-    public static readonly double F = 486.1327;
-    /** blue cadmium line at 479.9914nm */
-    public static readonly double F_ = 479.9914; // F'
-    /** blue mercury line at 435.8343nm */
-    public static readonly double g = 435.8343;
-    /** violet mercury line at 404.6561nm */
-    public static readonly double h = 404.6561;
-    /** ultraviolet mercury line at 365.0146nm */
-    public static readonly double i = 365.0146;
-    
-    /** Create a spectral line with specified wavelen and
-     intensity. */
-    public SpectralLine (double wavelen, double intensity) {
-        this._wavelen = wavelen;
-        this._intensity = intensity;
-    }
-
-    /** get spectral line wavelen */
-    public double get_wavelen ()
+     This class can be used to describe a spectral line. It contains
+     constants for wave length of standard rays
+     */
+    public class SpectralLine
     {
-        return _wavelen;
-    }
-    /** get spectral line intensity */
-    public double get_intensity ()
-    {
-        return _intensity;
-    }
-    /** get rgb color associated spectral line, ignore intensity */
-    public Rgb get_color ()
-    {
-        return get_wavelen_color (_wavelen);
-    }
+        double _wavelen;
+        double _intensity;
 
-    /** get rgb color associated with wavelen */
-    public static Rgb get_wavelen_color (double wl) {
-        // based on algorithm from Dan Bruton
-        // (www.physics.sfasu.edu/astro/color.html)
-        // http://www.physics.sfasu.edu/astro/color/spectra.html
+        /** red colored line at 645nm */
+        public static readonly double red = 645.0;
+        /** green colored line at 510nm */
+        public static readonly double green = 510.0;
+        /** blue colored line at 440nm */
+        public static readonly double blue = 440.0;
 
-        if (wl < 380.0 || wl > 780.0)
-            return Rgb.rgb_black;
+        /** infrared mercury line at 1013.98nm */
+        public static readonly double t = 1013.98;
+        /** infrared cesium line at 852.11nm */
+        public static readonly double s = 852.11;
+        /** red helium line at 706.5188nm */
+        public static readonly double r = 706.5188;
+        /** red hydrogen line at 656.2725nm */
+        public static readonly double C = 656.2725;
+        /** red cadmium line at 643.8469nm */
+        public static readonly double C_ = 643.8469; // C'
+        /** yellow sodium line at 589.2938nm */
+        public static readonly double D = 589.2938;
+        /** yellow helium line at 587.5618nm */
+        public static readonly double d = 587.5618;
+        /** green mercury line at 546.074nm */
+        public static readonly double e = 546.074;
+        /** blue hydrogen line at 486.1327nm */
+        public static readonly double F = 486.1327;
+        /** blue cadmium line at 479.9914nm */
+        public static readonly double F_ = 479.9914; // F'
+        /** blue mercury line at 435.8343nm */
+        public static readonly double g = 435.8343;
+        /** violet mercury line at 404.6561nm */
+        public static readonly double h = 404.6561;
+        /** ultraviolet mercury line at 365.0146nm */
+        public static readonly double i = 365.0146;
 
-        double s = 1.0;
-
-        if (wl < 420.0)
-            s = 0.3 + 0.7 * (wl - 380.0f) / 40.0;
-        else if (wl > 700.0)
-            s = 0.3 + 0.7 * (780.0 - wl) / 80.0;
-
-        if (wl < 510.0)
+        /** Create a spectral line with specified wavelen and
+         intensity. */
+        public SpectralLine(double wavelen, double intensity)
         {
-            if (wl < 490.0)
-            {
-                if (wl < 440.0)
-                    // 380 to 440
-                    return new Rgb (s * -(wl - 440.0) / 60.0, 0.0, s, 1.0);
-          else
-                // 440 to 490
-                return new Rgb (0.0, s * (wl - 440.0) / 50.0, s, 1.0);
-            }
-            else
-                // 490 to 510
-                return new Rgb (0.0, s, s * -(wl - 510.0) / 20.0, 1.0);
+            this._wavelen = wavelen;
+            this._intensity = intensity;
         }
-        else
+
+        /** get spectral line wavelen */
+        public double get_wavelen()
         {
-            if (wl < 645.0)
+            return _wavelen;
+        }
+        /** get spectral line intensity */
+        public double get_intensity()
+        {
+            return _intensity;
+        }
+        /** get rgb color associated spectral line, ignore intensity */
+        public Rgb get_color()
+        {
+            return get_wavelen_color(_wavelen);
+        }
+
+        /** get rgb color associated with wavelen */
+        public static Rgb get_wavelen_color(double wl)
+        {
+            // based on algorithm from Dan Bruton
+            // (www.physics.sfasu.edu/astro/color.html)
+            // http://www.physics.sfasu.edu/astro/color/spectra.html
+
+            if (wl < 380.0 || wl > 780.0)
+                return Rgb.rgb_black;
+
+            double s = 1.0;
+
+            if (wl < 420.0)
+                s = 0.3 + 0.7 * (wl - 380.0f) / 40.0;
+            else if (wl > 700.0)
+                s = 0.3 + 0.7 * (780.0 - wl) / 80.0;
+
+            if (wl < 510.0)
             {
-                if (wl < 580.0)
-                    // 510 to 580
-                    return new Rgb (s * (wl - 510.0) / 70.0, s, 0.0, 1.0);
-          else
-                // 580 to 645
-                return new Rgb (s, s * -(wl - 645.0) / 65.0, 0.0, 1.0);
+                if (wl < 490.0)
+                {
+                    if (wl < 440.0)
+                        // 380 to 440
+                        return new Rgb(s * -(wl - 440.0) / 60.0, 0.0, s, 1.0);
+                    else
+                        // 440 to 490
+                        return new Rgb(0.0, s * (wl - 440.0) / 50.0, s, 1.0);
+                }
+                else
+                    // 490 to 510
+                    return new Rgb(0.0, s, s * -(wl - 510.0) / 20.0, 1.0);
             }
             else
             {
-                // 645 to 780
-                return new Rgb (s, 0.0, 0.0, 1.0);
+                if (wl < 645.0)
+                {
+                    if (wl < 580.0)
+                        // 510 to 580
+                        return new Rgb(s * (wl - 510.0) / 70.0, s, 0.0, 1.0);
+                    else
+                        // 580 to 645
+                        return new Rgb(s, s * -(wl - 645.0) / 65.0, 0.0, 1.0);
+                }
+                else
+                {
+                    // 645 to 780
+                    return new Rgb(s, 0.0, 0.0, 1.0);
+                }
             }
         }
-    }
 
-}
+    }
 
 }
