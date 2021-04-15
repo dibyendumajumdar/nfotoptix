d06516978905ee290e9d0d972968be14555fcb91
Dibyendu Majumdar <mobile@majumdar.org.uk>
2021-04-15 20:14:18 +0100

    reformat

--- Library/src/Redukti.NFotoptix/Air.cs
+++ Library/src/Redukti.NFotoptix/Air.cs
@@ -26,111 +26,124 @@ Original GNU Optical License and Authors are as follows:
 
 using System;
 
-namespace Redukti.Nfotopix {
-
-/**
- * Air optical material model
- * This class models optical properties of air. Refractive index
- * of air depends on temperature and pressure.
- * <p>
- * This class provides different formulas:
- * <ol>
- * <li>AirBirch94 : described in Birch, Metrologia, 1994, 31, 315.</li>
- * <li>AirKohlrausch68 : described in F. Kohlrausch, Praktische Physik, 1968, 1, 408}</li>
- * </ol>
- * <p>
- * Global variables air and std_air are available with
- * default parameters and Birch model.
- */
-public class Air : MaterialBase {
+namespace Redukti.Nfotopix
+{
 
     /**
-     * Standard air pressure is 101325 @em Pa
+     * Air optical material model
+     * This class models optical properties of air. Refractive index
+     * of air depends on temperature and pressure.
+     * <p>
+     * This class provides different formulas:
+     * <ol>
+     * <li>AirBirch94 : described in Birch, Metrologia, 1994, 31, 315.</li>
+     * <li>AirKohlrausch68 : described in F. Kohlrausch, Praktische Physik, 1968, 1, 408}</li>
+     * </ol>
+     * <p>
+     * Global variables air and std_air are available with
+     * default parameters and Birch model.
      */
-    static readonly double std_pressure = 101325.0;
-    public static readonly Air std_air = new Air(AirFormula.AirBirch94Formula);
-    public static readonly Air air = new Air(AirFormula.AirBirch94Formula);
+    public class Air : MaterialBase
+    {
 
-    public readonly double _pressure;
-    public readonly AirFormula airFormula;
+        /**
+         * Standard air pressure is 101325 @em Pa
+         */
+        static readonly double std_pressure = 101325.0;
+        public static readonly Air std_air = new Air(AirFormula.AirBirch94Formula);
+        public static readonly Air air = new Air(AirFormula.AirBirch94Formula);
 
-    public Air(AirFormula formula, double pressure) : base("air", 20.0) {
-        this.airFormula = formula;
-        this._pressure = pressure;
-    }
+        public readonly double _pressure;
+        public readonly AirFormula airFormula;
 
-    public Air(AirFormula formula, double pressure, double temp) : base("air", temp) {
-        this.airFormula = formula;
-        this._pressure = pressure;
-    }
-
-    public Air(AirFormula formula): this(formula, std_pressure) {
-    }
-
-
-    
-    override public bool is_opaque() {
-        return false;
-    }
-
-    
-    override public bool is_reflecting() {
-        return false;
-    }
-
-    
-    override public double get_internal_transmittance(double wavelen, double thickness) {
-        // FIXME find a formula
-        return 1.0;
-    }
-
-    
-    override public double get_refractive_index(double wavelen) {
-        switch (airFormula) {
-            case AirFormula.AirBirch94Formula: {
-                // Birch, Metrologia, 1994, 31, 315
-
-                // _temperature in celsius
-                // _pressure in pascal
-
-                double s2 = MathUtils.square(1 / (wavelen / 1000.0));
-
-                double ref1
-                        = /*1.0 +*/ 1e-8
-                        * (+8342.54 + 2406147.0 / (130.0 - s2) + 15998.0 / (38.9 - s2));
-
-                return 1.0
-                        + (ref1 /*- 1.0*/)
-                        * (_pressure
-                        * (1.0
-                        + _pressure * (60.1 - 0.972 * _temperature)
-                        * 1e-10))
-                        / (96095.43 * (1.0 + 0.003661 * _temperature));
-            }
-
-            case AirFormula.AirKohlrausch68Formula: {
-                // F. Kohlrausch, Praktische Physik, 1968, 1, 408
-
-                double w2 = MathUtils.square(wavelen / 1000.0);
-
-                double nref = 1.0
-                        + (+6432.8 + (2949810.0 * w2) / (146.0 * w2 - 1.0)
-                        + (25540.0 * w2) / (41.0 * w2 - 1.0))
-                        * 1e-8;
-
-                return 1.0
-                        + (((nref - 1.0) * (_pressure / std_pressure))
-                        / (1.0 + (_temperature - 15.0) * 0.0034785));
-            }
+        public Air(AirFormula formula, double pressure) : base("air", 20.0)
+        {
+            this.airFormula = formula;
+            this._pressure = pressure;
         }
-        throw new InvalidOperationException();
-    }
 
-    override public double get_extinction_coef(double wavelen) {
-        // FIXME find a formula
-        return 0.0;
-    }
+        public Air(AirFormula formula, double pressure, double temp) : base("air", temp)
+        {
+            this.airFormula = formula;
+            this._pressure = pressure;
+        }
 
-}
+        public Air(AirFormula formula) : this(formula, std_pressure)
+        {
+        }
+
+
+
+        override public bool is_opaque()
+        {
+            return false;
+        }
+
+
+        override public bool is_reflecting()
+        {
+            return false;
+        }
+
+
+        override public double get_internal_transmittance(double wavelen, double thickness)
+        {
+            // FIXME find a formula
+            return 1.0;
+        }
+
+
+        override public double get_refractive_index(double wavelen)
+        {
+            switch (airFormula)
+            {
+                case AirFormula.AirBirch94Formula:
+                    {
+                        // Birch, Metrologia, 1994, 31, 315
+
+                        // _temperature in celsius
+                        // _pressure in pascal
+
+                        double s2 = MathUtils.square(1 / (wavelen / 1000.0));
+
+                        double ref1
+                                = /*1.0 +*/ 1e-8
+                                * (+8342.54 + 2406147.0 / (130.0 - s2) + 15998.0 / (38.9 - s2));
+
+                        return 1.0
+                                + (ref1 /*- 1.0*/)
+                                * (_pressure
+                                * (1.0
+                                + _pressure * (60.1 - 0.972 * _temperature)
+                                * 1e-10))
+                                / (96095.43 * (1.0 + 0.003661 * _temperature));
+                    }
+
+                case AirFormula.AirKohlrausch68Formula:
+                    {
+                        // F. Kohlrausch, Praktische Physik, 1968, 1, 408
+
+                        double w2 = MathUtils.square(wavelen / 1000.0);
+
+                        double nref = 1.0
+                                + (+6432.8 + (2949810.0 * w2) / (146.0 * w2 - 1.0)
+                                + (25540.0 * w2) / (41.0 * w2 - 1.0))
+                                * 1e-8;
+
+                        return 1.0
+                                + (((nref - 1.0) * (_pressure / std_pressure))
+                                / (1.0 + (_temperature - 15.0) * 0.0034785));
+                    }
+            }
+            throw new InvalidOperationException();
+        }
+
+        override public double get_extinction_coef(double wavelen)
+        {
+            // FIXME find a formula
+            return 0.0;
+        }
+
+    }
 
 }
