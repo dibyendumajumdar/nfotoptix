169e89dbd94f64810bf2d141c8b7d1e2d208104c
Dibyendu Majumdar <mobile@majumdar.org.uk>
2021-04-15 22:17:21 +0100

    reformat

--- Library/src/Redukti.NFotoptix/Distribution.cs
+++ Library/src/Redukti.NFotoptix/Distribution.cs
@@ -25,91 +25,94 @@ Original GNU Optical License and Authors are as follows:
 
 using System;
 
-namespace Redukti.Nfotopix {
+namespace Redukti.Nfotopix
+{
 
-/**
- Ray distribution pattern descriptor
+    /**
+     Ray distribution pattern descriptor
 
- This class describes distribution pattern and ray density used
- for light ray distribution over surfaces during light
- propagation.
+     This class describes distribution pattern and ray density used
+     for light ray distribution over surfaces during light
+     propagation.
 
- Ray density is expressed as average number of rays along
- surface radius.
- */
-public class Distribution {
-
-    Pattern _pattern;
-    int _radial_density;
-    double _scaling;
-
-    /** Creates a distribution pattern with specified pattern,
-     radial ray density and scaling.
-
-     The scaling ratio parameter may be used to avoid
-     distributing rays too close to the surface edge. */
-    public Distribution (Pattern pattern,
-                         int radial_density,
-                         double scaling)
+     Ray density is expressed as average number of rays along
+     surface radius.
+     */
+    public class Distribution
     {
-        if (radial_density < 1)
-            throw new InvalidOperationException ("ray distribution radial density must be greater than 1");
-        this._pattern = pattern;
-        this._radial_density = radial_density;
-        this._scaling = scaling;
-    }
 
-    public Distribution() {
-        _pattern = Pattern.DefaultDist;
-        _radial_density = 5;
-        _scaling = 0.999;
-    }
+        Pattern _pattern;
+        int _radial_density;
+        double _scaling;
 
-    void set_pattern (Pattern p)
-    {
-        _pattern = p;
-    }
+        /** Creates a distribution pattern with specified pattern,
+         radial ray density and scaling.
 
-    public Pattern get_pattern ()
-    {
-        return _pattern;
-    }
-
-    public int get_radial_density ()
-    {
-        return _radial_density;
-    }
-
-    void set_radial_density (int density)
-    {
-        _radial_density = density;
-    }
-
-    public double get_scaling ()
-    {
-        return _scaling;
-    }
-
-    void set_scaling (double margin)
-    {
-        _scaling = margin;
-    }
-
-    public void set_uniform_pattern ()
-    {
-        switch (_pattern)
+         The scaling ratio parameter may be used to avoid
+         distributing rays too close to the surface edge. */
+        public Distribution(Pattern pattern,
+                             int radial_density,
+                             double scaling)
         {
-            case Pattern.SagittalDist:
-            case Pattern.MeridionalDist:
-            case Pattern.CrossDist:
-                _pattern = Pattern.DefaultDist;
-                    break;
-            default:
-                    break;
-
+            if (radial_density < 1)
+                throw new InvalidOperationException("ray distribution radial density must be greater than 1");
+            this._pattern = pattern;
+            this._radial_density = radial_density;
+            this._scaling = scaling;
         }
-    }
 
-}
+        public Distribution()
+        {
+            _pattern = Pattern.DefaultDist;
+            _radial_density = 5;
+            _scaling = 0.999;
+        }
+
+        void set_pattern(Pattern p)
+        {
+            _pattern = p;
+        }
+
+        public Pattern get_pattern()
+        {
+            return _pattern;
+        }
+
+        public int get_radial_density()
+        {
+            return _radial_density;
+        }
+
+        void set_radial_density(int density)
+        {
+            _radial_density = density;
+        }
+
+        public double get_scaling()
+        {
+            return _scaling;
+        }
+
+        void set_scaling(double margin)
+        {
+            _scaling = margin;
+        }
+
+        public void set_uniform_pattern()
+        {
+            switch (_pattern)
+            {
+                case Pattern.SagittalDist:
+                case Pattern.MeridionalDist:
+                case Pattern.CrossDist:
+                    _pattern = Pattern.DefaultDist;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+    }
 
 }
