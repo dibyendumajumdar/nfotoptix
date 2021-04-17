37bb2d24c0495d1bb5607ca33217ab1701354ebe
Dibyendu Majumdar <mobile@majumdar.org.uk>
2021-04-17 10:41:42 +0100

    Refactoring

--- Library/src/Redukti.NFotoptix/Abbe.cs
+++ Library/src/Redukti.NFotoptix/Abbe.cs
@@ -28,10 +28,8 @@ using System;
 
 namespace Redukti.Nfotopix
 {
-
     public class Abbe : Dielectric
     {
-
         /**
          * Specify Abbe number type used by the @ref Abbe class
          */
@@ -41,8 +39,8 @@ namespace Redukti.Nfotopix
             AbbeVe, //< Abbe number of Fraunhofer @em e line
         }
 
-        double _n, _q, _a;
-        AbbeFormula _m;
+        readonly double _n, _q, _a;
+        readonly AbbeFormula _m;
 
         /**
          * Create an abbe glass model
@@ -57,7 +55,6 @@ namespace Redukti.Nfotopix
 
         Abbe(AbbeFormula m, double n, double v) : this(m, n, v, 0.0)
         {
-
         }
 
 
@@ -74,16 +71,16 @@ namespace Redukti.Nfotopix
 
                 case AbbeFormula.AbbeVd:
                     f = (_a * -6.11873891971188577088 + 1.17752614766485175224)
-                            + (_a * 18.27315722388047447566 + -8.93204522498095698779) / wl
-                            + (_a * -14.55275321129051135927 + 7.91015964461522003148) / w2
-                            + (_a * 3.48385106908642905310 + -1.80321117937358499361) / w3;
+                        + (_a * 18.27315722388047447566 + -8.93204522498095698779) / wl
+                        + (_a * -14.55275321129051135927 + 7.91015964461522003148) / w2
+                        + (_a * 3.48385106908642905310 + -1.80321117937358499361) / w3;
                     break;
 
                 case AbbeFormula.AbbeVe:
                     f = (_a * -5.70205459879993181715 + 0.73560912822245871912)
-                            + (_a * 17.84619335902774039937 + -8.71504708663084315390) / wl
-                            + (_a * -14.30050903441605747446 + 7.77787634432116181671) / w2
-                            + (_a * 3.41225047218704347074 + -1.76619259848202947438) / w3;
+                        + (_a * 17.84619335902774039937 + -8.71504708663084315390) / wl
+                        + (_a * -14.30050903441605747446 + 7.77787634432116181671) / w2
+                        + (_a * 3.41225047218704347074 + -1.76619259848202947438) / w3;
                     break;
 
                 default:
@@ -93,5 +90,4 @@ namespace Redukti.Nfotopix
             return _n + _q * f;
         }
     }
-
 }
